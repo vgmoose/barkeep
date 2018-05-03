6331f466ba7798394f6539df068f4c723efce10d
vgmoose <me@vgmoose.com>
2018-05-03 11:48:55 -0400

    login/login and protected dashboard

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+# third-party imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+
+
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+
+# after the db variable initialization
+login_manager = LoginManager()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    print config_name
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    Bootstrap(app)
+
+    from app import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+    return app
