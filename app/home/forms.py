10367b2439ae38969aff103f1f88f9b574e3b68d
vgmoose <me@vgmoose.com>
2018-05-14 03:02:16 -0400

    add initial package editing and creation

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, SubmitField, ValidationError, SelectField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, URL, Optional
 
-import os
+import os, re
+
+pattern = re.compile("^\w+$")   # alphanumeric and underscore
+
 DATA = "./data/"
 
 class AddRepoForm(FlaskForm):
@@ -11,7 +14,7 @@ class AddRepoForm(FlaskForm):
     (makes a folder on disk)
     """
     name = StringField('Folder name', validators=[DataRequired()])
-    
+
     submit = SubmitField('Create')
 
     def validate_name(self, field):
@@ -19,3 +22,29 @@ class AddRepoForm(FlaskForm):
             raise ValidationError("Folder name cannot be 'add'")
         if os.path.isdir(DATA + field.data.lower()):
             raise ValidationError("A repo with that name already exists")
+
+class AddPackageForm(FlaskForm):
+    name = StringField("Package Name", validators=[DataRequired()])
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    category = SelectField("Category", choices=[("game", "Games"), ("emu", "Emulators"), ("tool", "Tools"), ("concept", "Concepts"), ("loader", "Loaders")])
+    version = StringField("Version", validators=[DataRequired()])
+    short_desc = StringField("Short Description", validators=[DataRequired()])
+    details = TextAreaField("Long Details", render_kw={'class': 'form-control', 'rows': 15})
+    url = StringField("URL", validators=[URL()])
+    license = StringField("License")
+
+
+    submit = SubmitField('Create')
+    	# "title": "AppStore NX",
+    	# "author": "vgmoose",
+    	# "category": "loader",
+    	# "version": "1.0",
+    	# "description": "Download and manage homebrew apps",
+    	# "details": "The Homebrew App Store NX is graphical frontend to the get package manager for downloading and managing homebrew on the Nintendo Switch. This is a successor to the Wii U Homebrew App Store.\\n\\nFor further information you can visit the Source link below or browse to the GBAtemp release post here : https://gbatemp.net/threads/switch-homebrew-appstore.493086/\\n",
+    	# "url": "https://github.com/vgmoose/appstorenx",
+    	# "license": "GPLv3 License"
+
+    def validate_name(self, field):
+        if not re.match(pattern, field.data):
+            raise ValidationError("Package name must be alphanumeric, with no spaces")
--- app/home/views.py
+++ app/home/views.py
@@ -2,7 +2,9 @@ from flask import flash, redirect, render_template, url_for, request
 from flask_login import login_required
 
 from . import home
-from forms import AddRepoForm
+from forms import AddRepoForm, AddPackageForm
+
+from collections import defaultdict
 
 import os, json
 DATA = "./data/"
@@ -20,11 +22,16 @@ def dashboard():
     """
     Render the dashboard template on the /dashboard route
     """
-    
+
     # get the directories in the data folder
     # (each directory represents another repo)
     repos = os.listdir(DATA)
-    
+
+    for repo in repos:
+        # remove it if it's not a directory
+        if not os.path.isdir(DATA + repo):
+            repos.remove(repo)
+
     return render_template('home/dashboard.html', title="Dashboard", repos=repos)
 
 @home.route('/dashboard/add', methods=['GET', 'POST'])
@@ -35,45 +42,48 @@ def newrepo():
     """
     form = AddRepoForm()
     if form.validate_on_submit():
-        
+
         # make the directory for this package
         os.mkdir(DATA + form.name.data)
-        
+
         flash('Repo created successfully')
 
         # redirect to the login page
         return redirect(url_for('home.dashboard'))
 
     # load registration template
-    return render_template('home/add.html', form=form, title='Add Repo')
+    return render_template('home/add.html', form=form, title='Local Repo')
 
 @home.route('/dashboard/<folder>', methods=['GET', 'POST'])
 @login_required
 def getrepo(folder):
-    
+
     # check if repo doesn't exist
     if not os.path.isdir(DATA + folder):
         content = "Repo '" + folder +"' does not exist<br><br><a href='/dashboard'>Return to dashboard</a>"
         return render_template('simple.html', content=content)
-    
+
     # load packages from json
-    
+
     JSON = DATA + folder + "/repo.json"
-    
+
     # touch json file if it doesn't exist
     if not os.path.exists(JSON):
         with open(JSON, 'w+') as f:
             content = {"packages": []}
             json.dump(content, f)
-    
+
+        # make packages directory
+        os.mkdir(DATA + folder + "/packages")
+
     packages = {}
-    
+
     with open(JSON) as f:
         packages = json.load(f)
-        
+
     if "packages" not in packages:
         packages["packages"] = []
-        
+
     # a delete is coming in
     if request.method == 'POST' and request.form['action'] == 'delete':
         # make sure there are no packages
@@ -85,27 +95,53 @@ def getrepo(folder):
             return redirect(url_for("home.dashboard"))
         else:
             flash("Repo must have 0 packages to delete")
-    
+
     return render_template('home/getrepo.html', title="%s Repo" % folder, repo=folder, packages=packages["packages"], count=len(packages["packages"]))
 
 
 
-@home.route('/dashboard/<folder>/add', methods=['GET', 'POST'])
+@home.route('/dashboard/<folder>/<package>', methods=['GET', 'POST'])
 @login_required
-def modifypackage():
+def modifypackage(folder, package):
     """
     The form for making or editing a new package
     """
     form = AddPackageForm()
+
+    # if it's not "add", we should try to load an existing package
+    if package != "add":
+        INFO_PATH = DATA + folder + "/packages/" + package + "/info.json"
+
+        # try to open the file
+        try:
+            with open(INFO_PATH, "r") as inp:
+                d = json.load(inp)
+                contents = defaultdict(str, d)
+
+                # read in all properties
+                form.name.data       = package
+                form.name.render_kw = {'disabled':''}
+                form.title.data      = contents["title"]
+                form.author.data     = contents["author"]
+                form.category.data   = contents["category"]
+                form.version.data    = contents["version"]
+                form.short_desc.data = contents["description"]
+                form.details.data    = contents["details"].replace("\\n", "\n")
+                form.url.data        = contents["url"]
+                form.license.data    = contents["license"]
+        except IOError:
+            return render_template('404.html', resource=package), 404
+
     if form.validate_on_submit():
-        
+
         # make the directory for this package
-        os.mkdir(DATA + form.name.data)
-        
-        flash('Repo created successfully')
+        PKG_PATH = DATA + folder + "/" + form.name.data + "/"
+        os.mkdir(PKG_PATH)
+
+        flash('Package created successfully')
 
         # redirect to the login page
         return redirect(url_for('home.dashboard'))
 
     # load registration template
-    return render_template('home/add.html', form=form, title='Add Repo')
+    return render_template('home/add.html', form=form, title='Package', package=package)
